7e638964231116660a08ec235005eca417f46546
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-08-23 12:15:58 +0300

    crud, forms, helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,35 @@
 Rails.application.routes.draw do
-  
   root "main#index"
+
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'categories/new'
+  # get 'categories/edit'
+  # get 'categories/delete'
+
+  resources :categories do
+    member do
+      get :delete
+    end
+  end
+
+  resources :tasks do
+    member do
+      get :delete
+    end
+  end
+  
+  
 
   match 'about', to: "main#about", via: :get
   match 'hello', to: "main#hello", via: :get
 
-  get 'main/index'
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'tasks/edit'
+  # get 'main/index'
+  # get 'tasks/index'
+  # get 'tasks/new'
+  # get 'tasks/edit'
+  # get 'tasks/show'
+  # get 'tasks/delete'
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_201616) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_053843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -21,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_201616) do
     t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
 end
